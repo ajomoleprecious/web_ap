c873c7a49c71d84031193111bd1038c8a157e59c
Precious <preciouscoolcool@gmail.com>
2023-10-27 15:12:28 +0200

    update 27/10 #7

--- js/contact.js
+++ js/contact.js
@@ -1,19 +1,18 @@
 
-const coord = [51.23040011948502, 4.414190136845983];
+const coord = [51.230230, 4.414230];
+var icon = L.icon({
+    iconUrl: 'assets/icons/icon-ap.png',
 
-var map = L.map('map').setView(coord, 17);
-
-var greenIcon = L.icon({
-    iconUrl: 'leaf-green.png',
-    shadowUrl: 'leaf-shadow.png',
-
-    iconSize:     [38, 95], // size of the icon
+    iconSize:     [60, 40], // size of the icon
     iconAnchor:   [22, 94], // point of the icon which will correspond to marker's location
     popupAnchor:  [-3, -76] // point from which the popup should open relative to the iconAnchor
 });
 
+var map = L.map('map').setView(coord, 17);
+
 L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
     maxZoom: 19,
-    attribution: '&copy; <a href="http://localhost:3000/index.html">WatchMovies</a>'
+    attribution: '&copy; <a href="/index.html">WatchMovies</a>'
 }).addTo(map);
+L.marker(coord, {icon: icon}).addTo(map).bindPopup('WatchMovies').openPopup();
 

d990b55e31f610b2ff2ac77cf4722f43bd02c97c
ajomole_precious <precious.ajomole@student.kdg.be>
2023-12-18 00:15:38 +0100

    added fetch api for clients

---
+++ js/klanten.js
@@ -0,0 +1,21 @@
+const klantenlist = document.querySelector('.klantenlist');
+
+fetch('https://randomuser.me/api/?results=5')
+.then(response => response.json())
+.then(data => {
+    let klanten = data.results;
+    
+    klanten.forEach(klant => {
+        const klantItem = `
+        <div class="klant">
+            <img src="${klant.picture.large}" alt="Foto van ${klant.name.first} ${klant.name.last}">
+            <div class="klantinfo">
+                <h3>${klant.name.first} ${klant.name.last}</h3>
+                <p>${klant.email}</p>
+                <p>Woont in: ${klant.location.city}</p>
+            </div>
+            </div>
+            `;
+            klantenlist.innerHTML += klantItem;
+    });
+})
